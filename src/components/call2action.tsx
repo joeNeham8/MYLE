b0600e5d696dc5d6f4fffcf0bbaaa1f4269ea7df
joeNeham8 <166697328+joeNeham8@users.noreply.github.com>
2024-11-06 21:33:18 +0530

    Add files via upload

---
+++ src/components/call2action.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './call2action.css';
+
+const CallToAction: React.FC = () => {
+  return (
+    <section id="pricing">
+      <h2>Start Your Free Trial Today!</h2>
+      <a href="#signup" className="cta-btn">Sign Up</a>
+    </section>
+  );
+};
+
+export default CallToAction;
